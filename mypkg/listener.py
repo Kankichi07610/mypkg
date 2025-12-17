de6e6d601b805332849fcb4f66218ad08c7825a4
Kanta Ogasawara <ogakanta05@gmail.com>
2025-12-17 17:48:45 +0900

    Listener修正

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -5,8 +5,11 @@ from std_msgs.msg import Int16
 
 rclpy.init()
 node = Node("listener")
+
 def cb(msg):
     global node
     node.get_logger().info("Listen: %d" % msg.data)
-    def main():
-    pub = node.create_subscription(Int16, "countup", cb, 10) 　　　                  rclpy.spin(node)
+
+def main():
+    pub = node.create_subscription(Int16, "countup", cb, 10)
+    rclpy.spin(node)
