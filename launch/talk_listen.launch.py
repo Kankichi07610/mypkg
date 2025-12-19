402ae3fd9344a04b2add79243afe17e016b4c827
Kanta Ogasawara <ogakanta05@gmail.com>
2025-12-20 02:23:12 +0900

    add

--- launch/talk_listen.launch.py
+++ launch/talk_listen.launch.py
@@ -3,19 +3,18 @@ import launch.actions
 import launch.substitutions
 import launch_ros.actions
 
+
 def generate_launch_description():
 
     talker = launch_ros.actions.Node(
         package='mypkg',
-        executable='talker',)
+        executable='talker',
+        )
 
     listener = launch_ros.actions.Node(
         package='mypkg',
         executable='listener',
-        output='screen',
-    )
+        output='screen'
+        )
 
-    return launch.LaunchDescription([
-        talker,
-        listener
-    ])
+    return launch.LaunchDescription([talker, listener])
